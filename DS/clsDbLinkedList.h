c1eaa74d0a0bd5657e430bb40a02095bf884de50
osama mohamoud <wwwhdosama@gmail.com>
2026-02-12 18:38:01 +0200

    Auto commit: Thu Feb 12 18:38:00 EST 2026

--- DS/clsDbLinkedList.h
+++ DS/clsDbLinkedList.h
@@ -1,21 +1,10 @@
 #include <iostream>
 
 template <class T>
-class clsDbLinkedList; //template class declration 
-
-template <class T>
-class DbLinkedListInterface
-{
-    virtual void InsertAtTheBeginning(T Value) = 0;
-    virtual void PrintList() = 0;
-    virtual 
-};
-
-template <class T>
-class clsDbLinkedList : public DbLinkedListInterface<T>
+class InterfaceDbLinkedList // contract / interface / abstract class
 {
 
-public:
+public: // i put Node Here Because Of The pure virtual Methods That Return Node(class) value
     class Node
     {
     public:
@@ -31,8 +20,19 @@ public:
         };
     };
 
+
+    virtual void InsertAtTheBeginning(T Value) = 0;
+    virtual void PrintList() = 0;
+    virtual Node *Find(T value) = 0; // Find by Value
+};
+
+template <class T>
+class clsDbLinkedList : public InterfaceDbLinkedList<T>
+{
+    using node = typename InterfaceDbLinkedList<T>::Node; // to avoid writing unnecessary syntax every time 
+
 private:
-    Node *Head;
+    typename InterfaceDbLinkedList<T>::Node *Head;
 
 public:
     clsDbLinkedList()
@@ -41,27 +41,47 @@ public:
     };
 
     // Insert At The Beginning
+
     void InsertAtTheBeginning(T Value) override
     {
-        Node *NNode = new Node(Value); // create new node
-        NNode->Prev = nullptr;         // as it will be the first node
+
+        typename InterfaceDbLinkedList<T>::Node *NNode = new typename InterfaceDbLinkedList<T>::Node(Value); // create new node
+
+        NNode->Prev = nullptr; // as it will be the first node
 
         if (this->Head != nullptr) // there is next node
         {
             NNode->Next = Head;
+            NNode->Next->Prev = NNode;
             this->Head = NNode;
             return;
         };
         NNode->Next = nullptr;
+
         this->Head = NNode;
     };
 
     void PrintList() override
     {
-        Node *Temp = this->Head;
+        typename InterfaceDbLinkedList<T>::Node *Temp = this->Head;
 
         while (Temp != nullptr)
         {
             std::cout << Temp->Value << std::endl;
             Temp = Temp->Next; // move to the next node
-       
+        }
+    };
+
+    // return the address of the found node
+    typename InterfaceDbLinkedList<T>::Node *Find(T Value) override
+    {
+        typename InterfaceDbLinkedList<T>::Node *Temp = Head;
+        while (Temp != nullptr)
+        {
+            if (Temp->Value == Value)
+                return Temp;
+            Temp = Temp->Next; // move to the next node
+        };
+        return nullptr;
+    };
+};
--- DS/test.cc
+++ DS/test.cc
@@ -4,10 +4,18 @@ using namespace std;
 int main()
 {
     clsDbLinkedList<int> Db1;
+  
     Db1.InsertAtTheBeginning(10);
     Db1.InsertAtTheBeginning(20);
     Db1.InsertAtTheBeginning(30);
     Db1.InsertAtTheBeginning(40);
 
-    Db1.PrintList();
+      Db1.PrintList();
+
+    clsDbLinkedList<int>::Node * N1 = Db1.Find(20);
+
+     if(N1 !=nullptr) cout<<"\nN1 Found!\n";
+     else cout<<"\nDidn't Find\n";
+
+     cout<<N1->Value <<endl; 
 }
