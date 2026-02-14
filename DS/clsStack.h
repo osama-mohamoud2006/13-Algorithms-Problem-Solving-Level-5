2791eee14ac6a34f6a23920d2d28af3baead45cb
osama mohamoud <wwwhdosama@gmail.com>
2026-02-15 00:12:56 +0200

    Auto commit: Sun Feb 15 00:12:55 EST 2026

--- DS/clsStack.h
+++ DS/clsStack.h
@@ -1,19 +1,8 @@
-#include "../DS/clsDbLinkedList.h"
+#include "../DS/clsQueue.h"
 
 template <class T>
-class StackInterface
-{
-public:
-    virtual bool Empty() = 0;
-    virtual T Top() = 0;
-    virtual int Size() = 0;
-    virtual void Print() = 0;
-    virtual void Push(T Value) = 0;
-    virtual void Pop() = 0;
-};
 
-template <class T>
-class clsStack : StackInterface<T>
+class clsStack : clsQueue<T> // reuse the queue again and change its behaviour to cope with stack
 {
     // LIFO = Last In First Out
     // this class is just adapter
@@ -49,10 +38,56 @@ public:
         return Underlying.GetItem(0);
     };
 
-    bool Empty() override
-    {
-        return Underlying.IsEmpty();
-    };
-
 
 };
+
+
+
+
+
+// Another Solution /// 
+
+// template <class T>
+// class clsStack : StackInterface<T>
+// {
+//     // LIFO = Last In First Out
+//     // this class is just adapter
+//     // linked list is just container
+//     // so totally it is container adapter
+
+// private:
+//     clsDbLinkedList<T> Underlying;
+
+// public:
+//     void Push(T Value) override
+//     {
+//         Underlying.InsertAtTheBeginning(Value);
+//     };
+
+//     void Pop() override
+//     {
+//         Underlying.DeleteTheFirstNode();
+//     };
+
+//     void Print() override
+//     {
+//         Underlying.PrintList();
+//     };
+
+//     int Size() override
+//     {
+//         return Underlying.Size();
+//     };
+
+//     T Top() override
+//     {
+//         return Underlying.GetItem(0);
+//     };
+
+//     bool Empty() override
+//     {
+//         return Underlying.IsEmpty();
+//     };
+
+
+// };
