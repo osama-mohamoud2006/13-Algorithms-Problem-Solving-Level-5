b82fbca0ac36b39be681280a33fa0a15a95ece17
osama mohamoud <wwwhdosama@gmail.com>
2026-02-16 16:27:25 +0200

    Auto commit: Mon Feb 16 16:27:25 EST 2026

--- DS/clsDynamicArray.h
+++ DS/clsDynamicArray.h
@@ -7,20 +7,25 @@ public:
     virtual bool IsEmpty() = 0;
     virtual int ArraySize() = 0;
     virtual void PrintArrayItems() = 0;
+    virtual void Resize(int Size) = 0;
 };
 
 template <class T>
 class clsDynamicArray : public InterfaceclsDynamicArray<T>
 {
+private:
+    T *TempPtr = nullptr;
+
 protected:
     int _Length = 0;
 
 public:
-    T *ArrPtr = nullptr_t;          // Pointer To Allocate Memory
+    T *ArrPtr = nullptr;            // Pointer To Allocate Memory
     clsDynamicArray(int Length = 0) // Constructor
     {
-        if (0 >= Length)
-            return;
+        if (0 > Length) // negative nums
+            this->_Length = 0;
+
         this->_Length = Length;
         ArrPtr = new T[Length]; // Dynamic Array
     };
@@ -29,19 +34,19 @@ public:
     {
         if (Index >= this->_Length || 0 > Index)
             return false;
-            
+
         *(ArrPtr + Index) = Value;
-        return true; 
+        return true;
     };
 
     bool IsEmpty() override
     {
-        return (this->_Length==0);
+        return (this->_Length == 0);
     };
 
     int ArraySize() override
     {
-        return this->_Length - 1;
+        return this->_Length;
     };
 
     void PrintArrayItems() override
@@ -52,8 +57,24 @@ public:
         }
     };
 
+private:
+    void CopyArr(T Arr1, int size)
+    {
+        TempPtr = new T[size]; // temp dynamic array to copy elements to it
+        for (int i = 0; i < size; i++)
+        {
+            TempPtr[i] = Arr1[i]; // copy element by element
+        };
+    };
+
+    void Resize(int size) override 
+    {
+
+    };
+
     ~clsDynamicArray()
     {
-        delete ArrPtr[];
+        delete[] ArrPtr;
+        delete[] TempPtr ;
     };
 };
