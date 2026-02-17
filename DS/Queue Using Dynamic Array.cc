8a69a26593d33309da011b44b7a6ae755aff1a5c
osama mohamoud <wwwhdosama@gmail.com>
2026-02-17 21:52:31 +0200

    Auto commit: Tue Feb 17 21:52:31 EST 2026

---
+++ DS/Queue Using Dynamic Array.cc
@@ -0,0 +1,56 @@
+#include "../DS/clsDynamicArray.h"
+
+template <class T>
+class InterfaceQueue
+{
+public:
+    virtual int Size() = 0;
+    virtual T Front() = 0;
+    virtual T Back() = 0;
+    virtual void Print() = 0;
+    virtual void Push(T Value) = 0;
+    virtual void Pop() = 0;
+};
+
+template <class T>
+class clsQueue : public InterfaceQueue<T>
+{
+    // This Class Is Just Adapter
+    // Dynamic Array Is Just Underlying Container
+protected:
+    clsDynamicArray<T> Underlying;
+
+public:
+    void Push(T Value) override
+    {
+        Underlying.InsertAtTheEnd(Value);
+    };
+
+    void Pop() override
+    {
+        Underlying.DeleteFirstItem();
+    };
+
+    void Print() override
+    {
+        Underlying.PrintArrayItems();
+    };
+
+    T Back() override // Last Element
+    {
+        Underlying.GetItem(ArraySize() - 1);
+    };
+
+    T Front() override // First Element
+    {
+        Underlying.GetItem(0);
+    };
+
+    int Size() override
+    {
+        return Underlying.ArraySize();
+    };
+
+    bool Empty(){
+        return Underlying.IsEmpty() ';'}
+};
