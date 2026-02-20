d54b41d37e72df8ad5c3f2609fa156acaeb207fb
osama mohamoud <wwwhdosama@gmail.com>
2026-02-20 15:01:25 +0200

    Auto commit: Fri Feb 20 15:01:24 EST 2026

--- QueueLine/clsQueueLine.h
+++ QueueLine/clsQueueLine.h
@@ -1,4 +1,5 @@
 #include <queue>
+#include <stack>
 #include "../10-OOP Concepts/Project 3/clsDate.h"
 #include <string>
 class InterfaceQueueLine
@@ -20,7 +21,7 @@ private:
     int TotalTickets;
     int ServedClients;
     int WaitingClients;
-    int NumOfClient;
+    int NumOfClientForId;
 
 public:
     clsQueueLine(std::string Perfix, int ExpectedTime) // constructor
@@ -41,23 +42,113 @@ private:
         std::string ClientId;
 
     public:
-        clsTicket(int NumOfClient , std::string Perfix)
+        clsTicket(int NumOfClient, std::string Perfix)
         {
             TimeDate = clsDate::GetLocalDateAndTime();
-            ClientId= Perfix+std::to_string(NumOfClient);
+            ClientId = Perfix + std::to_string(NumOfClient);
+        };
+
+        std::string GetClientId()
+        {
+            return this->ClientId;
+        };
+
+        std::string GetDateTimeDetails()
+        {
+            return this->TimeDate;
         };
     };
 
-    std::queue<clsTicket> Line; // FIFO
+    std::queue<clsTicket> QueueOfLine; // FIFO
 
 public:
     void IssueTicket() override
     {
         TotalTickets++; // as new client issued a ticket
         WaitingClients++;
-        NumOfClient++;
-        clsTicket NewTicket(NumOfClient,this->Perfix); // ticket info object includes time and client id
+        NumOfClientForId++;
+        clsTicket NewTicket(NumOfClientForId, this->Perfix); // ticket info object includes time and client id
 
-        Line.push(NewTicket);
+        QueueOfLine.push(NewTicket);
     };
+
+    void PrintInfo() override
+    {
+        std::cout << "\t\t\t\t\t___________________________\n";
+        std::cout << "Perfix: " << this->Perfix << std::endl;
+        std::cout << "Total Tickets: " << this->TotalTickets << std::endl;
+        std::cout << "Served Clients: " << this->ServedClients << std::endl;
+        std::cout << "Waiting Clients: " << this->WaitingClients << std::endl;
+        std::cout << "\t\t\t\t\t___________________________\n";
+    };
+
+    void PrintTicketsLineRtl() override
+    {
+        std::queue<clsTicket> TempLine = this->QueueOfLine;
+        std::string strLine = "";
+        while (!TempLine.empty())
+        {
+            strLine += TempLine.front().GetClientId() + "<---  "; // get client that that will be served at first  id form queue object
+            TempLine.pop();
+        };
+        strLine.pop_back(); // to remove the last "<---"
+        std::cout << strLine << std::endl;
+    };
+
+    void PrintTicketsLineLtr() override
+    {
+        std::queue<clsTicket> TempLine = this->QueueOfLine;
+        std::stack<clsTicket> TempStack; // to reverse queue
+        std::string strLine = "";
+
+        // Reverse Queue
+        while (!TempLine.empty())
+        {
+            TempStack.push(TempLine.front()); // push the front queue element to stack
+            TempLine.pop();
+        };
+
+        while (!TempStack.empty()) // as stack follow LIFO
+        {
+            strLine += TempStack.top().GetClientId() + "--->  "; // get client that that will be served at first  id form queue object
+            TempStack.pop();
+        };
+
+        strLine.pop_back(); // to remove the last "<---"
+        std::cout << strLine << std::endl;
+    };
+
+private:
+    void PrintSingleTicket(clsTicket Client)
+    {
+        std::cout << "\t\t\t\t\t\t" << Client.GetClientId() << "\n";
+        std::cout << "\t\t\t\t\t" << Client.GetDateTimeDetails() << "\n";
+        std::cout << "Waiting Clients= " << this->WaitingClients << "\n";
+        std::cout << "Serve Time In: " << WaitingClients * ExpectedTime << std::endl;
+    };
+
+public:
+    void PrintAllTickets() override
+    {
+        std::queue<clsTicket> TempLine = this->QueueOfLine;
+        std::cout << "\n\t\t\t\t\tTickets\n";
+        std::cout << "\t\t\t\t_____________________________\n";
+
+        while (!TempLine.empty())
+        {
+            PrintSingleTicket(TempLine.front());
+            std::cout << "\t\t\t\t_____________________________\n";
+            TempLine.pop();
+        };
+    };
+
+    void ServeNextClient() override
+    {
+        this->WaitingClients--;
+        this->ServedClients++;
+        this->QueueOfLine.pop(); 
+    };
+
+
+
 };
---
+++ QueueLine/main.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "../QueueLine/clsQueueLine.h"
+using namespace std; 
+
+int
